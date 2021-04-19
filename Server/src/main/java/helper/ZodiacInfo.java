d77cda4ee9da5e8035ec9181176d24a0fa1c909c
biancacibotaru <bianca.cibotaru@yahoo.com>
2021-04-19 20:24:49 +0300

    Added ZodiacInfo class

---
+++ Server/src/main/java/helper/ZodiacInfo.java
@@ -0,0 +1,36 @@
+package helper;
+
+public class ZodiacInfo {
+    private String name;
+    private String startDate;
+    private String endDate;
+    public ZodiacInfo(String name, String startDate, String endDate){
+        this.name=name;
+        this.startDate=startDate;
+        this.endDate=endDate;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(String startDate) {
+        this.startDate = startDate;
+    }
+
+    public String getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(String endDate) {
+        this.endDate = endDate;
+    }
+}
